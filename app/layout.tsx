33d64c4dc8f7e9bc6f4f0d143c5a437396df92ce
edward sreeman <104483305+SreeXman@users.noreply.github.com>
2023-09-19 10:59:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
           <Analytics />
         </ThemeProvider>
+        <script>
+	var beamer_config = {
+		product_id : 'XftVgXsi58481' //DO NOT CHANGE: This is your product code on Beamer
+	};
+</script>
+<script type="text/javascript" src="https://app.getbeamer.com/js/beamer-embed.js" defer="defer"></script>
       </body>
       
     </html>
